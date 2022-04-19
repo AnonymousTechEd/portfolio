78d1eadc6de2e8f6d07ccd9cb7dccb7c82f94b75
Eduard Faus <44145601+Eduard-Faus@users.noreply.github.com>
2022-04-19 19:26:58 -0400

    first partt

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Navbar from "../Components/navbar.svelte";
+</script>
+<Navbar />
+<br>
+<br>
+<div class="box">
+    <div class="columns is-centered is-mobile">
+        <div class="column">
+            <figure class="image is-1by1">
+                <img alt="me" src="../Assets/me.png">
+            </figure>
+        </div>
+        <div class="column">
+            <h1 class ="title is-1 "> Hi! I'm Eduard</h1>
+            <br>
+            <p class="subtitle">
+                I am a freshman in Belen Jesuit Preparatory School who loves to code
+
+            </p>
+        </div>
+    </div>
+</div>
