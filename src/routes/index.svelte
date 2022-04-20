c8786062a78271b4204a493a50c0d25eb1e1756b
Eduard Faus <44145601+Eduard-Faus@users.noreply.github.com>
2022-04-19 20:21:45 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,10 @@
 <Navbar />
 <br>
 <br>
-<div class="box">
-    <div class="columns is-centered is-mobile">
+<div class="box container is-halfheight is-half" style="height:50%;">
+    <div class="columns is- half is-centered is-mobile is-halfheight">
         <div class="column">
-            <figure class="image is-1by1">
+            <figure class="image is-16x16 is-size-6" style=" height: auto; width: auto; max-width: 100%; max-height: 100%;">
                 <img alt="me" src="../Assets/me.png">
             </figure>
         </div>
@@ -15,9 +15,53 @@
             <h1 class ="title is-1 "> Hi! I'm Eduard</h1>
             <br>
             <p class="subtitle">
-                I am a freshman in Belen Jesuit Preparatory School who loves to code
-
+                I am currently a freshman in Belen Jesuit Preparatory School (Miami, FL) who loves to code.
+                Originally from Barcelona, Spain, I speak English, Spanish and Catalan.<br><br>
+                I am a self-taught programmer. Although I mainly make websites, I also enjoy making games, apps, and anything that involves artificial intelligence.
+                <br>
+                <br>
+                I recently decided to begin the <strong> #100daysofcode </strong> challenge and post my results here.
+                I will also post other projects that I am working on and things im intersted about. Check them out!
+                <br>
+                <br>
+                Social Media:
             </p>
         </div>
     </div>
+</div>
+<div class="container box has-text-centered">
+    <h1 class ="title is-1 ">My Projects</h1>
+    <br>
+    <p class="subtitle">
+        <li> vLex Chrome Extension (2020, 13 years old) </li>
+        <li> vLex Chrome Extension </li>
+       <li> vLex Chrome Extension  </li>
+    </p>
+</div>
+<div class="container box has-text-centered">
+    <h1 class ="title is-1 ">#100 Days of Code</h1>
+    <br>
+    <p class="subtitle">
+        <li> vLex Chrome Extension
+        <li> vLex Chrome Extension
+    <li> vLex Chrome Extension
+    </p>
+</div>
+<div class="container box has-text-centered">
+    <h1 class ="title is-1 ">#My Articles</h1>
+    <br>
+    <p class="subtitle">
+        <li> vLex Chrome Extension
+        <li> vLex Chrome Extension
+    <li> vLex Chrome Extension
+    </p>
+</div>
+<div class="container box has-text-centered">
+    <h1 class ="title is-1 ">#Books I Like</h1>
+    <br>
+    <p class="subtitle">
+        <li> vLex Chrome Extension
+        <li> vLex Chrome Extension
+    <li> vLex Chrome Extension
+    </p>
 </div>
